81641b7c0ea9a47ccd1198858f714b2f3b534cd4
watanabe3tipapa <twpoet@nifty.com>
2024-02-22 20:19:17 +0900

    adding

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'watanabe3tipapa Blog';
+export const SITE_DESCRIPTION = 'Welcome to my Repository';
