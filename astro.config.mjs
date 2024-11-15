2d25674741136c511c1de56762966d842d8a057e
Toru Watanabe <twpoet@nifty.com>
2024-11-15 10:07:58 +0900

    Astro_build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github-papa.watanabe3ti.com',
+	site: 'https://watanabe3tipapa.github.io',
 	base: '/watanabe3tipapa',
 	integrations: [mdx(), sitemap()],
 });
